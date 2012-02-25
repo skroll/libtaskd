ffcf5038edfd96444fc6ff5ec05968b57bb54bd9
Paul Beckingham <paul@beckingham.net>
2012-02-25 09:01:12 -0500

    Code Cleanup
    - Fixed cmake issues.
    - Added man page pre-preocessing.
    - Added unit tests.
    - Added README.

---
+++ test/sample.t.cpp
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+// kronisk - Date/Time/Duration Library
+//
+// Copyright 2010 - 2012, Göteborg Bit Factory.
+// Copyright 2010 - 2012, Paul Beckingham, Federico Hernandez.
+// All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <taskd.h>
+#include <test.h>
+
+////////////////////////////////////////////////////////////////////////////////
+int main (int argc, char** argv)
+{
+  UnitTest t (1);
+
+  t.skip ("No tests implemented");
+
+  return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
