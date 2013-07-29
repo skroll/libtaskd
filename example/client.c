e8846a183857b00dd9f53fec87b2f104896f9bb1
Paul Beckingham <paul@beckingham.net>
2013-07-28 23:24:46 -0400

    Example
    - Improved client sample.

--- example/client.c
+++ example/client.c
@@ -3,48 +3,52 @@
 
 int main (int argc, char** argv)
 {
+  // Authentication provides all necessary credentials.
+  char* server       = "localhost:6544";
+  char* organization = "Public";
+  char* user         = "USER";
+  char* password     = "4b821202-cf12-4965-9b1f-ab5413eb2e07";
+  char* cert         = "./client.cert.pem";
+  taskd_authenticate (server, organization, user, password, cert);
+
   // This task represents a locally-modified task that needs to be synced with
-  // the server.
+  // the server.  It is added to the list of local changes.
   char* local_task = "{"
                      "\"description\":\"This is a sample task\","
                      "\"entry\":\"20130721T175511Z\","
                      "\"status\":\"pending\","
                      "\"uuid\":\"b7f1a997-9192-4a85-af5a-3eb58e76d573\""
                      "}";
-
-  // Initially this is blank, but is updated on every sync call.
-  char sync_key[64] = "";
-
-  taskd_authenticate ("localhost:6544", "PUBLIC", "John Doe", "key", "/path/to/cert.pem");
-
-  // For each local task that changed since the last sync, add it to the sync
-  // request.
   taskd_add_local_change (local_task);
 
   // Perform the encryption, communication and sync.  The sync key is modified.
-  int status = taskd_sync (sync_key);
+  // The local change list is sent and cleared.  The remote change list is
+  // populated.
+  char sync_key[40] = "";
+  if (taskd_sync (sync_key) == 0)
+  {
+    printf ("new sync key = %s\n", sync_key);
 
-  // A non-zero status indicates something went wrong.
-  if (status)
+    // After a sync, there may be remote task changes from the server, that need
+    // to overwrite local tasks, or just be added to the list.  Simply query the
+    // remote changes until the function retuns fails.
+    char remote_task[512];
+    while (taskd_get_remote_change (remote_task, 512) == 0)
+      printf ("received %s\n", remote_task);
+
+    // Sometimes the server has a message for the client, which may notify the
+    // user of some maintenance downtime, for example.
+    char message[512];
+    if (taskd_server_message (message, 512) == 0)
+      printf ("message from the server: %s\n", message);
+  }
+  else
   {
     int code;
     char error[512];
-    taskd_error (&code, error, 512);
-    printf ("server error %s\n", error);
+    if (taskd_error (&code, error, 512) == 0)
+      printf ("error %s\n", error);
   }
-
-  // After a sync, there may be remote task changes from the server, that need
-  // to overwrite local tasks, or just added to the list.  Simply query the
-  // remote changes until the function retuns non-zero.
-  char remote_task[512];
-  while (taskd_get_remote_change (remote_task, 512) == 0)
-    printf ("received %s\n", remote_task);
-
-  // Sometimes the server has a message for the client, which may notify the
-  // user of some maintenance downtime, for example.
-  char message[512];
-  if (taskd_server_message (message, 512) == 0)
-    printf ("message from server: %s\n", message);
 
   return 0;
 }
